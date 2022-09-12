4d512fce6026b5d1e38747a0bc91683f5b27d0d1
qiuy <qiuy@learn.com>
2022-09-12 10:10:32 +0800

    second commit

--- src/main/java/com/qiuy/gittest/Main.java
+++ src/main/java/com/qiuy/gittest/Main.java
@@ -6,5 +6,6 @@ package com.qiuy.gittest;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
